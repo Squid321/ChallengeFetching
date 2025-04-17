9d1b1fa854cd63cfea4e37f45ae6a6642290cf8d
Haynes_Ethan <hayne382@hsd.k12.or.us>
2025-04-17 10:15:42 -0700

    fixing first set of bugs

---
+++ Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (transform.position.x < -25 ) {
+            Destroy(gameObject);
+        }
+        else if (transform.position.y < -50) {
+            Destroy(gameObject);
+        }
+    }
+}
